799a3d9aa666d5204fe9144f7c21a5bbcf19bf88
shyno <mr.shyno@bk.ru>
2021-06-16 01:24:19 +0300

    addPath

--- data/config/path/paths.sh
+++ data/config/path/paths.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 #example:
 #echo "pathToFileOrDirectory";
+
+
+#linux
+echo "/tmp";
+echo "/var/tmp";
+
+
+#android
+echo "/data/data/com.termux/files/usr/tmp";
+#echo "/sdcard/Telegram/'Telegram Audio'";
+#echo "/sdcard/Telegram/Telegram\Documents";
+#echo "/sdcard/Telegram/Telegram Images";
+#echo "/sdcard/Telegram/Telegram Video";
